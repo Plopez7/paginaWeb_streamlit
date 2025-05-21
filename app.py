d0e4b8c0be42b853678707cc0f0e33c1ff5b7321
Plopez7 <111075025+Plopez7@users.noreply.github.com>
2025-05-21 10:31:19 -0500

    primera grafica

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
+import plotly.express as px
 
 st.title("Mi tercera publicación")
 st.header("Introducción")
 st.write("Esta es la primera que me sale algo")
+
+st.write("Este es un gráfico de barras simple.")
+fig = px.bar(
+    x=["A", "B", "C"],
+    y=[1, 2, 3],
+    title="Gráfico de barras",
+)
+st.plotly_chart(fig)
